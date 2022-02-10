e64cee5dab1bcb93baf340eb7664c5a3a1e1ae13
Harshit Gajjar <hrstgajjar3@gmail.com>
2022-02-09 18:41:56 -0800

    feburay 2, 2022 class

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+class SearchBar extends Component {
+  // props has all values which is received from the parent
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      query: "Harshit",
+    };
+  }
+
+  onSearchChange(eve) {
+    console.log("searched changed", eve.target.value);
+    this.setState({
+      query: eve.target.value,
+    });
+  }
+
+  render() {
+    return (
+      <label htmlFor="">
+        Search:
+        <input
+          type="text"
+          value={this.state.query}
+          onChange={this.onSearchChange.bind(this)}
+        />
+      </label>
+    );
+  }
+}
+class TestLists extends Component {
+  tests = Array.from({ length: 5 }).map((d, i) => ({
+    name: `Test ${i}`,
+    length: Math.random() * 20,
+  }));
+
+  renderTests() {
+    return this.tests.map((test) => <li key={test.name}>{test.name}</li>);
+  }
+
+  render() {
+    return <ul>{this.renderTests()}</ul>;
+  }
+}
+
+class App extends React.Component {
+  render() {
+    return (
+      <div class="App-header">
+        <h1>Test Manager</h1>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Deserunt,
+          quia tempora cum pariatur officiis laborum nemo nam consectetur
+          distinctio unde nostrum repellat minus odio, quam ea magni dolorem,
+          impedit eum.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+        <SearchBar />
+        <h2>Current Tests</h2>
+        <TestLists />
+      </div>
+    );
+  }
 }
 
 export default App;
